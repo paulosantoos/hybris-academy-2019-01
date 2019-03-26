3280cc26149c2a7dbec6d4f002b7f90049e41271
Lucas Zarzur <lucas.zarzur@verti.net.br>
2019-03-26 15:50:24 -0300

    Melhorias OO

--- FH/src/AulaJava_1/Pedido.java
+++ FH/src/AulaJava_1/Pedido.java
@@ -1,16 +1,15 @@
 package AulaJava_1;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Pedido {
-	
+
 	private String nomeProduto;
 	private String precoProduto;
+	private String opcao;
+	private int numParcelas = 0;
+	int valorParcela = 0;
+	int valorTotal = 0;
+	int soma = 0;
 	private int codigoPedido = 1;
-
-	
 
 	public String getNomeProduto() {
 		return nomeProduto;
@@ -20,30 +19,91 @@ public class Pedido {
 		this.nomeProduto = nomeProduto;
 	}
 
-
 	public String getPrecoProduto() {
 		return precoProduto;
 	}
-
 
 	public void setPrecoProduto(String precoProduto) {
 		this.precoProduto = precoProduto;
 	}
 
+	public String getOpcao() {
+		return opcao;
+	}
+
+	public void setOpcao(String opcao) {
+		this.opcao = opcao;
+	}
+
+	public int getNumParcelas() {
+		return numParcelas;
+	}
+
+	public void setNumParcelas(int numParcelas) {
+		this.numParcelas = numParcelas;
+	}
 
 	public int getCodigoPedido() {
 		return codigoPedido;
 	}
 
-	
 	public void setCodigoPedido(int codigoPedido) {
 		this.codigoPedido = codigoPedido;
 	}
 
+	public int getSoma() {
+		return soma;
+	}
 
-	public void pagamentoParcela() { 
+	public void setSoma(int soma) {
+		this.soma = soma;
+	}
 
-		
-}
+	public int calculatValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(int valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public void defineParcelas() {
+
+		switch (opcao) {
+		case "1":
+			numParcelas = 1;
+			break;
+		case "2":
+			numParcelas = 2;
+			break;
+		case "3":
+			numParcelas = 3;
+			break;
+		case "exit":
+			System.out.println("Obrigado, volte sempre!");
+			System.exit(0);
+		default:
+				System.out.println("Opção inválida, tente novamente!");
+				System.exit(0);
+		}
+	}
+
+	public void calculaParcelas() {
+
+		valorParcela = valorTotal / numParcelas;
+
+		if (numParcelas == 1) {
+			System.out.println("Parcela 1: " + valorParcela);
+		} else if (numParcelas == 2) {
+			System.out.println("Parcela 1: " + valorParcela);
+			System.out.println("Parcela 2: " + valorParcela);
+		} else {
+			System.out.println("Parcela 1: " + valorParcela);
+			System.out.println("Parcela 2: " + valorParcela);
+			System.out.println("Parcela 3: " + valorParcela);
+		}
+
+
+	}
 
 }
--- FH/src/AulaJava_1/Principal.java
+++ FH/src/AulaJava_1/Principal.java
@@ -7,114 +7,65 @@ import java.lang.String;
 public class Principal {
 
 	public static void main(String[] args) {
-		
 
 		String[] itensPedido = new String[3];
 		String[] valoresPedido = new String[3];
-		
-		System.out.println("Olá, seja bem-vindo ao programa de "
-				+ "pedido de venda!");
-		
-		String produtos = "";
-		String precosProdutos = "";
+		System.out.println("Olá, seja bem-vindo ao programa de " + "pedido de venda!");
+
 		Scanner respostas = new Scanner(System.in);
-		
+
 		LocalDate dataAtual = LocalDate.now();
-		
-		
+
 		Cliente cliente = new Cliente();
-		
 		System.out.println("Digite o seu nome: ");
 		cliente.setNome(respostas.nextLine());
-		
+
 		System.out.println("\nDigite o seu endereço: ");
 		cliente.setEndereco(respostas.nextLine());
-		
 
 		Pedido pedido = new Pedido();
+		for (int i = 0; i < 3; i++) {
+			System.out.println("\nDigite o nome do produto: ");
+			pedido.setNomeProduto(respostas.nextLine());
+			itensPedido[i] = pedido.getNomeProduto();
 
-		for(int i=0;i<3;i++) {
-		System.out.println("\nDigite o nome do produto: ");
-		pedido.setNomeProduto(respostas.nextLine());
-		itensPedido[i] = pedido.getNomeProduto();
-		
-		
-		System.out.println("\nDigite o valor do produto: ");
-		pedido.setPrecoProduto(respostas.nextLine());
-		valoresPedido[i] = pedido.getPrecoProduto();
-		
+			System.out.println("\nDigite o valor do produto: ");
+			pedido.setPrecoProduto(respostas.nextLine());
+			valoresPedido[i] = pedido.getPrecoProduto();
+
 		}
 
-		
-		Pedido parcelas = new Pedido();
-		
 		System.out.println("\nEm quantas parcelas você deseja pagar?");
 		System.out.println("Digite um número entre 1 até 3: ");
 		System.out.println("Digite 'exit' para sair.");
-		String opcao = respostas.nextLine();
-		
-		int numParcelas = 0;
-		
-		switch (opcao) {
-        case "1":
-        	numParcelas = 1;
-        	break;
-        case "2":
-        	numParcelas = 2;
-        	break;
-        case "3":
-        	numParcelas = 3;
-        	break;
-        case "exit":
-        	System.out.println("Obrigado, volte sempre!");
-        	return;
-        default: 
-            	System.out.println("Opção inválida, tente novamente!");
-            	return;
+		pedido.setOpcao(respostas.nextLine());
+
+		pedido.defineParcelas();
+
+
+		System.out.println("\n ====== Recibo do pedido ====== ");
+		System.out.println("Código do pedido: " + pedido.getCodigoPedido());
+		System.out.println("A data de hoje é: " + dataAtual);
+		System.out.println("Seu nome é: " + cliente.getNome());
+		System.out.println("Seu endereço é: " + cliente.getEndereco());
+		System.out.println("Seu pedido foi: ");
+
+		for (int i = 0; i < itensPedido.length; i++) {
+			for (int j = 0; j < valoresPedido.length; j++) {
+			}
+			System.out.println("\nProduto: " + itensPedido[i] + " R$" + valoresPedido[i]);
 		}
-        	
 
-	System.out.println("\n ==== Recibo do pedido ==== ");
-	System.out.println("Código do pedido: " + pedido.getCodigoPedido());
-	System.out.println("A data de hoje é: " + dataAtual);
-	System.out.println("Seu nome é: " + cliente.getNome());
-	System.out.println("Seu endereço é: " + cliente.getEndereco());
-	System.out.println("Seu pedido foi: ");
-	
-	for(int i=0;i<itensPedido.length;i++) {
-		for(int j=0;j<valoresPedido.length;j++) {	
+		System.out.println("\nNúmero de parcelas: " + pedido.getNumParcelas());
+
+		for (int i = 0; i < valoresPedido.length; i++) {
+			pedido.valorTotal += Integer.parseInt(valoresPedido[i]); // conversao valor
+			pedido.soma += pedido.valorTotal;
 		}
-		System.out.println("\nProduto: " + itensPedido[i] + " R$" + valoresPedido[i]);
-	}
-	
-	int valorTotal = 0;
-	int soma = 0;
-	
-	for(int i=0; i<valoresPedido.length; i++) {
-	valorTotal += Integer.parseInt(valoresPedido[i]); // conversao valor
-	soma += valorTotal;
+
+		pedido.calculaParcelas();
+
+		System.out.println("\nValor total do pedido R$" + pedido.valorTotal);
 	}
 
-
-	int valorParcela = 0;
-	
-	valorParcela = valorTotal / numParcelas;
-	
-	System.out.println("\nNúmero de parcelas: " + numParcelas);
-	
-	if(numParcelas == 1) {
-		System.out.println("Parcela 1: " +valorParcela);
-	} else if (numParcelas == 2) {
-		System.out.println("Parcela 1: " +valorParcela);
-		System.out.println("Parcela 2: " +valorParcela);
-	} else {
-		System.out.println("Parcela 1: " +valorParcela);
-		System.out.println("Parcela 2: " +valorParcela);
-		System.out.println("Parcela 3: " +valorParcela);
-	}
-
-	System.out.println("\nValor total do pedido R$" +valorTotal);
-	}
-
-	
 }
