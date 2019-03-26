34005f7b318cd3c74f824ebcf958ef313c5b79f5
Lucas Zarzur <lucas.zarzur@verti.net.br>
2019-03-26 10:48:43 -0300

    Projeto Lucas.

---
+++ FH/src/AulaJava_1/Pedido.java
@@ -0,0 +1,49 @@
+package AulaJava_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Pedido {
+	
+	private String nomeProduto;
+	private String precoProduto;
+	private int codigoPedido = 1;
+
+	
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+
+	public String getPrecoProduto() {
+		return precoProduto;
+	}
+
+
+	public void setPrecoProduto(String precoProduto) {
+		this.precoProduto = precoProduto;
+	}
+
+
+	public int getCodigoPedido() {
+		return codigoPedido;
+	}
+
+	
+	public void setCodigoPedido(int codigoPedido) {
+		this.codigoPedido = codigoPedido;
+	}
+
+
+	public void pagamentoParcela() { 
+
+		
+}
+
+}
