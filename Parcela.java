95861988a3c3e4558f8d6d9c5eabc1e2228e612e
Leonardo <leomelniski@gmail.com>
2019-03-26 00:13:27 -0300

    fix exception do formato da data

--- Parcela.java
+++ Parcela.java
@@ -1,10 +1,50 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Parcela {
+    private int i = 0;
     private Date vencimento;
     private float valor;
 
     public  Parcela (){
 
     }
+
+    @Override
+    public String toString() {
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        String s = "";
+        s += "Parcela " + this.i + ": ";
+        s += formatter.format(this.vencimento);
+        s += String.format(" - R$ %2.2f", this.valor);
+        return s;
+    }
+
+    public Date getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(Date vencimento, int addMonth) {
+        i = addMonth + 1;
+        this.vencimento = this.addMonth(vencimento, i);
+    }
+
+    public Date addMonth(Date date, int i) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, i);
+        return cal.getTime();
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
 }
--- PedidoVenda.java
+++ PedidoVenda.java
@@ -1,3 +1,5 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,20 +10,26 @@ public class PedidoVenda {
     private Date date;
     private Integer numParcelas;
     Cliente cliente;
-    private List<Produto> itensPedido;
 
+    private List<Produto> itensPedido;
     private List<Parcela> parcelas;
 
     @Override
     public String toString() {
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
         String s = "";
         s += "Codigo do pedido: " + this.id + '\n';
-        s += "Data: " + this.date + '\n';
+        s += "Data: " + formatter.format(this.date) + '\n';
         s += this.cliente.toString() + '\n';
         s += "Produtos\n";
+
         for(Produto prod : this.itensPedido)
             s += prod.toString() + '\n';
         s += "Numero parcelas: " + this.numParcelas + "\n";
+
+        for(Parcela p : this.parcelas)
+            s += p.toString() + '\n';
 
         return s;
     }
@@ -33,6 +41,8 @@ public class PedidoVenda {
     public PedidoVenda(){
         id ++;
         this.itensPedido = new ArrayList<Produto>();
+        this.parcelas = new ArrayList<Parcela>();
+
     }
 
     public Date getDate() {
@@ -49,6 +59,15 @@ public class PedidoVenda {
 
     public void setNumParcelas(Integer numParcelas) {
         this.numParcelas = numParcelas;
+
+        float valor = (float) itensPedido.stream().mapToDouble(Produto::getValor).sum() / this.numParcelas;
+
+        for(int i = 0; i < this.numParcelas; i++){
+            Parcela p = new Parcela();
+            p.setValor(valor);
+            p.setVencimento(date, i);
+            parcelas.add(p);
+        }
     }
 
     public Cliente getCliente() {
